d7b13d03644bdb1c3ae8f693f219e0b3192fa278
TTLequals0 <dkrachtus@ttlequals0.com>
2019-10-26 17:27:06 -0500

    add region ap-northeast-3 add region me-south-1 change default image is Ubuntu 18 change removed support for older OSes change use tls-crypt change use predefined ffdhe2048 group Update README new regions remove custom ami support

--- scripts/openvpn-autoinstall.sh
+++ scripts/openvpn-autoinstall.sh
@@ -6,100 +6,82 @@
 # The script will use pre-defined values that can be changed manually in script.
 # This script also assume server is behind NAT.
 
-# Detect Debian users running the script with "sh" instead of bash
-if readlink /proc/$$/exe | grep -qs "dash"; then
-	echo "This script needs to be run with bash, not sh"
-	exit 1
-fi
-
-if [[ "$EUID" -ne 0 ]]; then
-	echo "Sorry, you must be root to run this"
-	exit 2
-fi
-
-if [[ ! -e /dev/net/tun ]]; then
-	echo "TUN is not available"
-	exit 3
-fi
-
-if grep -qs "CentOS release 5" "/etc/redhat-release"; then
-	echo "CentOS 5 is too old and not supported"
-	exit 4
-fi
-if [[ -e /etc/debian_version ]]; then
-	OS=debian
-	GROUPNAME=nogroup
-	RCLOCAL='/etc/rc.local'
-elif [[ -e /etc/centos-release || -e /etc/redhat-release ]]; then
-	OS=centos
-	GROUPNAME=nobody
-	RCLOCAL='/etc/rc.d/rc.local'
-else
-	echo "OS is not supported please run on Debian, Ubuntu or CentOS"
-	exit 5
-fi
-
 newclient () {
 	# Generates the custom client.ovpn
-	cp /etc/openvpn/client-common.txt ~/$1.ovpn
-	echo "<ca>" >> ~/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/ca.crt >> ~/$1.ovpn
-	echo "</ca>" >> ~/$1.ovpn
-	echo "<cert>" >> ~/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> ~/$1.ovpn
-	echo "</cert>" >> ~/$1.ovpn
-	echo "<key>" >> ~/$1.ovpn
-	cat /etc/openvpn/easy-rsa/pki/private/$1.key >> ~/$1.ovpn
-	echo "</key>" >> ~/$1.ovpn
-	echo "<tls-auth>" >> ~/$1.ovpn
-	cat /etc/openvpn/ta.key >> ~/$1.ovpn
-	echo "</tls-auth>" >> ~/$1.ovpn
+	{
+	cat /etc/openvpn/server/client-common.txt
+	echo "<ca>"
+	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	echo "</ca>"
+	echo "<cert>"
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$1".crt
+	echo "</cert>"
+	echo "<key>"
+	cat /etc/openvpn/server/easy-rsa/pki/private/"$1".key
+	echo "</key>"
+	echo "<tls-crypt>"
+	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+	echo "</tls-crypt>"
+	} > ~/"$1".ovpn
 }
 
 # Get external IP assumed behind NAT
 IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
 if [[ "$IP" = "" ]]; then
-		IP=$(wget -qO- ipv4.icanhazip.com)
+		IP=$(wget -4qO- "http://whatismyip.akamai.com/" || curl -4Ls "http://whatismyip.akamai.com/")
 fi
+
 PORT=1194
 CLIENT=aws_vpn
+GROUPNAME="nogroup"
 
-if [[ "$OS" = 'debian' ]]; then
-	apt-get update
-	apt-get install openvpn iptables openssl ca-certificates -y
-else
-	# distro is CentOS
-	yum install epel-release -y
-	yum -q list installed 'epel-release*' &>/dev/null || yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
-	yum install openvpn iptables openssl wget ca-certificates -y
-fi
+apt-get update
+apt-get update
+apt-get install openvpn iptables openssl ca-certificates -y
+
+
 # Remove old version of easy-rsa that was available by default in some openvpn packages
 if [[ -d /etc/openvpn/easy-rsa/ ]]; then
 	rm -rf /etc/openvpn/easy-rsa/
 fi
+
 # Get easy-rsa
-EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.6/EasyRSA-unix-v3.0.6.tgz'
-wget -O ~/easyrsa.tgz "$EASYRSAURL" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$EASYRSAURL"
+easy_rsa_url='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.5/EasyRSA-nix-3.0.5.tgz'
+wget -O ~/easyrsa.tgz "$easy_rsa_url" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$easy_rsa_url"
 tar xzf ~/easyrsa.tgz -C ~/
-mv ~/EasyRSA-v3.0.6/ /etc/openvpn/
-mv /etc/openvpn/EasyRSA-v3.0.6/ /etc/openvpn/easy-rsa/
-chown -R root:root /etc/openvpn/easy-rsa/
+mv ~/EasyRSA-3.0.5/ /etc/openvpn/server/
+mv /etc/openvpn/server/EasyRSA-3.0.5/ /etc/openvpn/server/easy-rsa/
+chown -R root:root /etc/openvpn/server/easy-rsa/
 rm -f ~/easyrsa.tgz
-cd /etc/openvpn/easy-rsa/
+cd /etc/openvpn/server/easy-rsa/
+
+# Workaround to remove unharmful error until easy-rsa 3.0.7
+# https://github.com/OpenVPN/easy-rsa/issues/261
+sed -i 's/^RANDFILE/#RANDFILE/g' /etc/openvpn/server/easy-rsa/openssl-easyrsa.cnf
+
 # Create the PKI, set up the CA, the DH params and the server + client certificates
 ./easyrsa init-pki
 ./easyrsa --batch build-ca nopass
-./easyrsa gen-dh
-./easyrsa build-server-full server nopass
-./easyrsa build-client-full $CLIENT nopass
-./easyrsa gen-crl
+EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-server-full server nopass
+EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$CLIENT" nopass
 EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
-# Move the stuff we need
-cp pki/ca.crt pki/private/ca.key pki/dh.pem pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn
+# Move some files 
+cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn/server
 # CRL is read with each client connection, when OpenVPN is dropped to nobody
-chown nobody:$GROUPNAME /etc/openvpn/crl.pem
-# Generate key for tls-auth
-openvpn --genkey --secret /etc/openvpn/ta.key
+chown nobody:"$GROUPNAME" /etc/openvpn/server/crl.pem
+# Generate key for tls-crypt
+openvpn --genkey --secret /etc/openvpn/server/tc.key
+
+# Use predefined ffdhe2048 group
+
+echo '-----BEGIN DH PARAMETERS-----
+MIIBCAKCAQEA//////////+t+FRYortKmq/cViAnPTzx2LnFg84tNpWp4TZBFGQz
++8yTnc4kmz75fS/jY2MMddj2gbICrsRhetPfHtXV/WVhJDP1H18GbtCFY2VVPe0a
+87VXE15/V8k1mE8McODmi3fipona8+/och3xWKE2rec1MKzKT0g6eXq8CrGCsyT7
+YdEIqUuyyOP7uWrat2DX9GgdT0Kj3jlN9K5W7edjcrsZCwenyO4KbXCeAvzhzffi
+7MA0BM0oNC9hkXL+nOmFg/+OTxIy7vKBg8P+OxtMb61zO7X8vC7CIAXFjvGDfRaD
+ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
+-----END DH PARAMETERS-----' > /etc/openvpn/dh.pem
 
 echo "port $PORT
 proto udp
@@ -118,7 +100,6 @@ ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
 echo 'push "redirect-gateway def1 bypass-dhcp"' >> /etc/openvpn/server.conf
 echo 'push "dhcp-option DNS 8.8.8.8"' >> /etc/openvpn/server.conf
 echo 'push "dhcp-option DNS 9.9.9.9"' >> /etc/openvpn/server.conf
-
 echo "keepalive 10 120
 cipher AES-256-CBC
 user nobody
@@ -128,73 +109,33 @@ persist-tun
 status openvpn-status.log
 verb 3
 mssfix 1200
-crl-verify crl.pem" >> /etc/openvpn/server.conf
+crl-verify crl.pem
+explicit-exit-notify" >> /etc/openvpn/server.conf
 
 # Enable net.ipv4.ip_forward for the system
-sed -i '/\<net.ipv4.ip_forward\>/c\net.ipv4.ip_forward=1' /etc/sysctl.conf
-if ! grep -q "\<net.ipv4.ip_forward\>" /etc/sysctl.conf; then
-	echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
-fi
-# Avoid reboot
+echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
-# Needed to use rc.local with some systemd distros
-if [[ "$OS" = 'debian' && ! -e $RCLOCAL ]]; then
-	echo '#!/bin/sh -e exit 0' > $RCLOCAL
-fi
-chmod +x $RCLOCAL
+
 # Set NAT for the VPN subnet
-iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to "$IP"
-sed -i "1 a\iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to $IP" $RCLOCAL
-if pgrep firewalld; then
-	# We don't use --add-service=openvpn because that would only work with
-	# the default port. Using both permanent and not permanent rules to
-	# avoid a firewalld reload.
-	firewall-cmd --zone=public --add-port=$PORT/udp
-	firewall-cmd --zone=trusted --add-source=10.8.0.0/24
-	firewall-cmd --permanent --zone=public --add-port=$PORT/udp
-	firewall-cmd --permanent --zone=trusted --add-source=10.8.0.0/24
-fi
-if iptables -L | grep -qE 'REJECT|DROP'; then
-	# If iptables has at least one REJECT rule, we asume this is needed.
-	# Not the best approach but I can't think of other and this shouldn't
-	# cause problems.
-	iptables -I INPUT -p udp --dport $PORT -j ACCEPT
-	iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
-	iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-	sed -i "1 a\iptables -I INPUT -p udp --dport $PORT -j ACCEPT" $RCLOCAL
-	sed -i "1 a\iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT" $RCLOCAL
-	sed -i "1 a\iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT" $RCLOCAL
-fi
+/sbin/iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $IP
+/sbin/iptables -I INPUT -p udp --dport $PORT -j ACCEPT
+/sbin/iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
+/sbin/iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+/sbin/iptables -t nat -D POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $IP
+/sbin/iptables -D INPUT -p udp --dport $PORT -j ACCEPT
+/sbin/iptables -D FORWARD -s 10.8.0.0/24 -j ACCEPT
+/sbin/iptables -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+
 # Some SELinux stuff
-if hash sestatus 2>/dev/null; then
-	if sestatus | grep "Current mode" | grep -qs "enforcing"; then
-		if [[ "$PORT" != '1194' ]]; then
-			# semanage isn't available in CentOS 6 by default
-			if ! hash semanage 2>/dev/null; then
-				yum install policycoreutils-python -y
-			fi
-			semanage port -a -t openvpn_port_t -p udp $PORT
-		fi
-	fi
+if sestatus 2>/dev/null | grep "Current mode" | grep -q "enforcing" && [[ "$PORT" != 1194 ]]; then
+	semanage port -a -t openvpn_port_t -p upd "$PORT"
 fi
+
 # restart OpenVPN
-if [[ "$OS" = 'debian' ]]; then
-	# check for systemd
-	if pgrep systemd-journal; then
-		systemctl restart openvpn@server.service
-	else
-		/etc/init.d/openvpn restart
-	fi
-else
-	if pgrep systemd-journal; then
-		systemctl restart openvpn@server.service
-		systemctl enable openvpn@server.service
-	else
-		service openvpn restart
-		chkconfig openvpn on
-	fi
-fi
-IP=$(wget -qO- ipv4.icanhazip.com)
+systemctl restart openvpn@server.service
+systemctl enable openvpn@server.service
+
+
 # client-common.txt is created so we have a template to add further users later
 echo "client
 dev tun
@@ -209,9 +150,11 @@ persist-tun
 remote-cert-tls server
 auth SHA512
 cipher AES-256-CBC
-setenv opt block-outside-dns
+ignore-unknown-option block-outside-dns
+block-outside-dns
 key-direction 1
-verb 3" > /etc/openvpn/client-common.txt
+verb 3" > /etc/openvpn/server/client-common.txt
+
 # Generates the custom client.ovpn
 newclient "$CLIENT"
 exit
