84b1e8cb8d63b51485fb39928a9c455c60ecb7e8
TTLequals0 <dkrachtus@ttlequals0.com>
2019-11-10 16:25:08 -0600

    Some more tweaks.

--- scripts/openvpn-autoinstall.sh
+++ scripts/openvpn-autoinstall.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # OpenVPN  automated installer for Debian, Ubuntu and CentOS
 
 # This script will work on Debian, Ubuntu, CentOS and probably other distros
@@ -9,27 +9,24 @@
 newclient () {
 	# Generates the custom client.ovpn
 	{
-	cat /etc/openvpn/server/client-common.txt
+	cat /etc/openvpn/client-common.txt
 	echo "<ca>"
-	cat /etc/openvpn/server/easy-rsa/pki/ca.crt
+	cat /etc/openvpn/easy-rsa/pki/ca.crt
 	echo "</ca>"
 	echo "<cert>"
-	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/server/easy-rsa/pki/issued/"$1".crt
+	sed -ne '/BEGIN CERTIFICATE/,$ p' /etc/openvpn/easy-rsa/pki/issued/"$1".crt
 	echo "</cert>"
 	echo "<key>"
-	cat /etc/openvpn/server/easy-rsa/pki/private/"$1".key
+	cat /etc/openvpn/easy-rsa/pki/private/"$1".key
 	echo "</key>"
 	echo "<tls-crypt>"
-	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/server/tc.key
+	sed -ne '/BEGIN OpenVPN Static key/,$ p' /etc/openvpn/tc.key
 	echo "</tls-crypt>"
 	} > ~/"$1".ovpn
 }
 
 # Get external IP assumed behind NAT
-IP=$(ip addr | grep 'inet' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -o -E '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
-if [[ "$IP" = "" ]]; then
-		IP=$(wget -4qO- "http://whatismyip.akamai.com/" || curl -4Ls "http://whatismyip.akamai.com/")
-fi
+IP=$(wget -4qO- "http://whatismyip.akamai.com/" || curl -4Ls "http://whatismyip.akamai.com/")
 
 PORT=1194
 CLIENT=aws_vpn
@@ -46,18 +43,18 @@ if [[ -d /etc/openvpn/easy-rsa/ ]]; then
 fi
 
 # Get easy-rsa
-easy_rsa_url='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.5/EasyRSA-nix-3.0.5.tgz'
-wget -O ~/easyrsa.tgz "$easy_rsa_url" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$easy_rsa_url"
+EASYRSAURL='https://github.com/OpenVPN/easy-rsa/releases/download/v3.0.6/EasyRSA-unix-v3.0.6.tgz'
+wget -O ~/easyrsa.tgz "$EASYRSAURL" 2>/dev/null || curl -Lo ~/easyrsa.tgz "$EASYRSAURL"
 tar xzf ~/easyrsa.tgz -C ~/
-mv ~/EasyRSA-3.0.5/ /etc/openvpn/server/
-mv /etc/openvpn/server/EasyRSA-3.0.5/ /etc/openvpn/server/easy-rsa/
-chown -R root:root /etc/openvpn/server/easy-rsa/
+mv ~/EasyRSA-v3.0.6/ /etc/openvpn/
+mv /etc/openvpn/EasyRSA-v3.0.6/ /etc/openvpn/easy-rsa/
+chown -R root:root /etc/openvpn/easy-rsa/
 rm -f ~/easyrsa.tgz
-cd /etc/openvpn/server/easy-rsa/
+cd /etc/openvpn/easy-rsa/
 
 # Workaround to remove unharmful error until easy-rsa 3.0.7
 # https://github.com/OpenVPN/easy-rsa/issues/261
-sed -i 's/^RANDFILE/#RANDFILE/g' /etc/openvpn/server/easy-rsa/openssl-easyrsa.cnf
+sed -i 's/^RANDFILE/#RANDFILE/g' /etc/openvpn/easy-rsa/openssl-easyrsa.cnf
 
 # Create the PKI, set up the CA, the DH params and the server + client certificates
 ./easyrsa init-pki
@@ -65,12 +62,12 @@ sed -i 's/^RANDFILE/#RANDFILE/g' /etc/openvpn/server/easy-rsa/openssl-easyrsa.cn
 EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-server-full server nopass
 EASYRSA_CERT_EXPIRE=3650 ./easyrsa build-client-full "$CLIENT" nopass
 EASYRSA_CRL_DAYS=3650 ./easyrsa gen-crl
-# Move some files 
-cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn/server
+# Move some files
+cp pki/ca.crt pki/private/ca.key pki/issued/server.crt pki/private/server.key pki/crl.pem /etc/openvpn/
 # CRL is read with each client connection, when OpenVPN is dropped to nobody
-chown nobody:"$GROUPNAME" /etc/openvpn/server/crl.pem
+chown nobody:"$GROUPNAME" /etc/openvpn/crl.pem
 # Generate key for tls-crypt
-openvpn --genkey --secret /etc/openvpn/server/tc.key
+openvpn --genkey --secret /etc/openvpn/tc.key
 
 # Use predefined ffdhe2048 group
 
@@ -83,7 +80,8 @@ YdEIqUuyyOP7uWrat2DX9GgdT0Kj3jlN9K5W7edjcrsZCwenyO4KbXCeAvzhzffi
 ssbzSibBsu/6iGtCOGEoXJf//////////wIBAg==
 -----END DH PARAMETERS-----' > /etc/openvpn/dh.pem
 
-echo "port $PORT
+echo "
+port $PORT
 proto udp
 dev tun
 sndbuf 0
@@ -93,7 +91,7 @@ cert server.crt
 key server.key
 dh dh.pem
 auth SHA512
-tls-auth ta.key 0
+tls-crypt tc.key
 topology subnet
 server 10.8.0.0 255.255.255.0
 ifconfig-pool-persist ipp.txt" > /etc/openvpn/server.conf
@@ -117,24 +115,15 @@ echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 # Set NAT for the VPN subnet
-/sbin/iptables -t nat -A POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $IP
-/sbin/iptables -I INPUT -p udp --dport $PORT -j ACCEPT
-/sbin/iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
-/sbin/iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-/sbin/iptables -t nat -D POSTROUTING -s 10.8.0.0/24 ! -d 10.8.0.0/24 -j SNAT --to $IP
-/sbin/iptables -D INPUT -p udp --dport $PORT -j ACCEPT
-/sbin/iptables -D FORWARD -s 10.8.0.0/24 -j ACCEPT
-/sbin/iptables -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
-
-# Some SELinux stuff
-if sestatus 2>/dev/null | grep "Current mode" | grep -q "enforcing" && [[ "$PORT" != 1194 ]]; then
-	semanage port -a -t openvpn_port_t -p upd "$PORT"
+iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to "$IP"
+if iptables -L | grep -qE 'REJECT|DROP'; then
+	iptables -I INPUT -p udp --dport $PORT -j ACCEPT
+	iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
+	iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 fi
 
 # restart OpenVPN
 systemctl restart openvpn@server.service
-systemctl enable openvpn@server.service
-
 
 # client-common.txt is created so we have a template to add further users later
 echo "client
@@ -152,8 +141,7 @@ auth SHA512
 cipher AES-256-CBC
 ignore-unknown-option block-outside-dns
 block-outside-dns
-key-direction 1
-verb 3" > /etc/openvpn/server/client-common.txt
+verb 3" > /etc/openvpn/client-common.txt
 
 # Generates the custom client.ovpn
 newclient "$CLIENT"
